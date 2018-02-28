1c92a6d7be543c8c7b92741994aac7da4b6497bd
Elena <elenamargayova@gmail.com>
2018-02-28 12:04:22 +0300

    Completed desktop and mobile styles.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,17 +1,2 @@
-var $=require('jquery');
-//var Person=require('./modules/Person');
-import Person from './modules/Person';
-
-class Adult extends Person{
-  payTaxes(){
-    console.log(this.name + "now owes $0 in taxes.");
-  }
-}
-
-alert ("This is a test !!!");
-var john=new Person("John Doe","blue");
-john.greet();
-
-var jane=new Adult("Jane Smith","orange");
-jane.greet();
-jane.payTaxes();
+import MobileMenu from './modules/MobileMenu';
+var mobileMenu=new MobileMenu();
